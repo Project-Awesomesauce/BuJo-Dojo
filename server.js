2a1f18354744ace8c3bd11dac230a4cda37c542b
ppreziotti <patpreziotti@gmail.com>
2017-06-07 20:33:43 -0400

    created initial api/html routes, required html routes in server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // =============================================================
 // Starts the server listening
