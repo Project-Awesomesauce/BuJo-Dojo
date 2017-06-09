69641a6532963d66716a4b70b68913a6ae145a98
Michael Eggers <mdeggers84@gmail.com>
2017-06-09 10:55:22 -0400

    :joy::imp: now with working tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,19 @@
 var Nightmare = require('nightmare');
 var should = require('chai').should();
-var $ = require('jquery');
 
 describe('BuJo Dojo', function () {
   this.timeout(15000);
 
   var url = 'http://localhost:8080';
-  describe('Start page', function () {
+
+  describe('Start Page', function () {
     it('should show form when loaded', function (done) {
-      new Nightmare({ show: true })
+      new Nightmare({})
         .goto(url)
         .evaluate(function () {
           return document.querySelectorAll('form').length;
         })
+        .end()
         .then(function (result) {
           result.should.equal(1);
           done();
@@ -20,19 +21,37 @@ describe('BuJo Dojo', function () {
     });
   });
 
-  describe('Daily Page', function () {
-    it('should display current tasks', function (done) {
+  describe('Add Button', function () {
+    it('should add an item to the end of task list', function (done) {
+      var expected = 'Test 42';
       new Nightmare({ show: true })
         .goto(url)
-        .type('[name=item]', 'Test Nightmare')
-        .click('[id=add-item')
-        .wait('#item-list')
+        .type('[name=item]', expected)
+        .click('#add-item')
+        .goto(url)
         .evaluate(function () {
-          // return document.querySelectorAll('#item-list .list-item')[0].innerText;
-          return $('.list-item')[0].innerText;
+          var items = document.querySelectorAll('#item-list .list-item');
+          return items[items.length - 1].innerText;
         })
         .then(function (result) {
-          result.should.equal('Test Nightmare');
+          result.should.equal(expected);
+          done();
+        });
+    });
+
+    it('should not allow a user to enter an empty string', function (done) {
+      var expected = '';
+      new Nightmare({})
+        .goto(url)
+        .type('[name=item]', expected)
+        .click('#add-item')
+        .goto(url)
+        .evaluate(function () {
+          var items = document.querySelectorAll('#item-list .list-item');
+          return items[items.length - 1].innerText;
+        })
+        .then(function (result) {
+          result.should.not.equal(expected);
           done();
         });
     });
