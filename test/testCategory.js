b8fa66b2db53a32fff8753640d722b07413946e4
Michael Eggers <mdeggers84@gmail.com>
2017-06-10 11:52:54 -0400

    finshed :cat: test

--- test/testCategory.js
+++ test/testCategory.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var assignCategory = require('../public/assets/js/index');
+var assignCategory = require('../public/assets/js/category');
 
 describe('BuJo Dojo Functions', function () {
   describe('Assign Category', function () {
@@ -13,6 +13,18 @@ describe('BuJo Dojo Functions', function () {
 
     it('should assign blank when note/complete', function () {
       expect(assignCategory('note', true)).to.equal('dash');
+    });
+
+    it('should assign bullet when task/not complete', function () {
+      expect(assignCategory('task', false).to.equal('bullet-crossed'));
+    });
+
+    it('should assign blank when event/not complete', function () {
+      expect(assignCategory('event', false)).to.equal('circle-crossed');
+    });
+
+    it('should assign blank when note/not complete', function () {
+      expect(assignCategory('note', false)).to.equal('dash-crossed');
     });
   });
 });
