1bfbfa9634e73a7af0e27d71a61d1feade8be3c0
ppreziotti <patpreziotti@gmail.com>
2017-06-10 13:16:31 -0400

    created assignCategory function (will need to change return values eventually)

---
+++ public/assets/js/category.js
@@ -0,0 +1,23 @@
+function assignCategory(category, complete) {
+  switch(category | complete) {
+  	case "task" | "false":
+  	  // Will eventually change the return values to actual font-awesome icon names
+  	  return "bullet"; 
+  	  break;
+    case "event" | "false":
+      return "circle";
+      break;
+	case "note" | "false":
+	  return "dash";
+	  break;
+	case "task" | "true":
+	  return "bullet-crossed";
+	  break;
+	case "event" | "true":
+	  return "circle-crossed";
+	  break;
+	case ("note" | "true"):
+	  return "dash-crossed";
+	  break;
+  }
+}
