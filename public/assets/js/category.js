1b89de1af64d37ca360bfbcabe53e49c459e14a7
Michael Eggers <mdeggers84@gmail.com>
2017-06-13 02:04:44 -0400

    edited category.js

--- public/assets/js/category.js
+++ public/assets/js/category.js
@@ -1,29 +1,22 @@
 var assignCategory = function (category, completed) {
-  if (category === "task" && completed === false) {
-  	return "fa-square-o"
+  if (category === 'task' && completed === false) {
+    return 'fa-square-o';
+  } else if (category === 'event' && completed === false) {
+    return 'fa-circle-o';
+  } else if (category === 'note' && completed === false) {
+    return 'fa-star-o';
+  } else if (category === 'research' && completed === false) {
+    return 'fa-eye';
+  } else if (category === 'task' && completed === true) {
+    return 'fa-check-square';
+  } else if (category === 'event' && completed === true) {
+    return 'fa-check-circle';
+  } else if (category === 'note' && completed === true) {
+    return 'fa-star';
+  } else if (category === 'research' && completed === true) {
+    return 'fa-eye-slash';
   }
-  else if (category === "event" && completed === false) {
-  	return "fa-circle-o"
-  }
-  else if (category === "note" && completed === false) {
-  	return "fa-star-o"
-  }
-  else if (category === "research" && completed === false) {
-  	return "fa-eye"
-  }
-  else if (category === "task" && completed === true) {
-  	return "fa-check-square"
-  }
-  else if (category === "event" && completed === true) {
-  	return "fa-check-circle"
-  }
-  else if (category === "note" && completed === true) {
-  	return "fa-star"
-  }
-  else if (category === "research" && completed === true) {
-  	return "fa-eye-slash"
-  }
-  return "no match";
-}
+  return 'no match';
+};
 
-//module.exports = assignCategory;
+module.exports = assignCategory;
